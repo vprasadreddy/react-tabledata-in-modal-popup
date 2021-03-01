368e5d63d27c5315407c09255fa64f18ac8c864a
vprasadreddy <reddy.rulz92@gmail.com>
2021-03-01 14:10:42 -0500

    code cleanup

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -6,32 +6,6 @@ import ContactList from "./ContactList";
 export class ContactApp extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selectedContact: {},
-      show: false
-    };
-    this.receiveContact = this.receiveContact.bind(this);
-    this.showModal = this.showModal.bind(this);
-    this.hideModal = this.hideModal.bind(this);
-  }
-
-  showModal() {
-    this.setState({
-      show: true
-    });
-  }
-
-  hideModal() {
-    this.setState({
-      show: false
-    });
-  }
-
-  receiveContact(contact) {
-    this.setState({
-      selectedContact: contact,
-      show: true
-    });
   }
 
   render() {
--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -61,28 +61,3 @@ export class ContactCard extends Component {
 }
 
 export default ContactCard;
-
-{
-  /* <div className="card" style={{ width: "18rem" }}>
-          <img
-            className="card-img-top"
-            src={this.props.selectedContact.picture["medium"]}
-            alt="Card image cap"
-          />
-          <div className="card-body">
-            <h5 className="card-title">{this.props.selectedContact.name.title}</h5>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              {this.props.selectedContact.email}
-            </li>
-            <li className="list-group-item">
-              {this.props.selectedContact.cell}
-            </li>
-          </ul>
-        </div> */
-}
